c23951ea38de27cfa8c94af1acd7a5623ea3103e
Saraswati Suvedi <harshitasuvedi11@gmail.com>
2026-01-29 14:18:10 +0530

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,8 @@
 # JWT authentication 
 # BEARER token le kr jata h 
-from data import app,CustomerData,predictionResponse,predict
+from data import CustomerData,predictionResponse,predict
+from fastapi import FastAPI
+app = FastAPI()
 # configuratins
 SECRET_KEY = 'sample key' #will be changed in production
 ALGORITHM = 'HS256'
