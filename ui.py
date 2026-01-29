80db2ee20cb14eb76099823aad31ca2798870703
harshita <harshitasuvedi11@gmail.com>
2026-01-29 17:21:11 +0530

    done

--- ui.py
+++ ui.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import requests
 
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://custom-churn-production.up.railway.app/"
 
 st.set_page_config(page_title="Churn Prediction App", layout="centered")
 st.title("📉 Customer Churn Prediction (JWT Secured)")
